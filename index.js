6e2f0ae3875e82986923274444350df307792141
Ruben Garcia <1029rubens@gmail.com>
2020-10-12 13:15:10 -0500

    Cambio de puerto

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use('/', routes() );
 
 
 const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 1029
+const port = process.env.PORT || 5000
 
 app.listen(port,host, ()=>{
     console.log('El servidor esta OK');
